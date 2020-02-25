916380556f8073c7d55d61ba483d54d5b9bdad54
Toshiaki Maki <tmaki@pivotal.io>
2020-02-26 00:01:20 +0900

    Cleanup

--- src/main/java/am/ik/lab/syaberu/SyaberuApplication.java
+++ src/main/java/am/ik/lab/syaberu/SyaberuApplication.java
@@ -2,10 +2,8 @@ package am.ik.lab.syaberu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class SyaberuApplication {
 
     public static void main(String[] args) {
