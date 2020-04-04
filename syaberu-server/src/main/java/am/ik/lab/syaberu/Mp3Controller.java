84d3b686a59bb3bdd2fcebf10a5397e7f2b15216
Toshiaki Maki <tmaki@pivotal.io>
2020-04-04 14:59:02 +0900

    Skip setting emotion if speaker is SHOW

--- syaberu-server/src/main/java/am/ik/lab/syaberu/Mp3Controller.java
+++ syaberu-server/src/main/java/am/ik/lab/syaberu/Mp3Controller.java
@@ -6,16 +6,14 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.RequestEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Base64;
 
 @RestController
+@CrossOrigin
 public class Mp3Controller {
     private final ApiKeyDecryptor apiKeyDecryptor;
     private final RestTemplate restTemplate;
@@ -25,7 +23,7 @@ public class Mp3Controller {
     public Mp3Controller(ApiKeyDecryptor apiKeyDecryptor, RestTemplateBuilder builder) {
         this.apiKeyDecryptor = apiKeyDecryptor;
         this.restTemplate = builder
-            .build();
+                .build();
     }
 
     @PostMapping(path = "syaberu.mp3")
@@ -36,19 +34,20 @@ public class Mp3Controller {
 
         log.info("text: {}, speaker: {}, emotion: {}", text, speaker, emotion);
         final RequestEntity<LinkedMultiValueMap<String, String>> request = RequestEntity
-            .post(URI.create("https://api.voicetext.jp/v1/tts"))
-            .headers(httpHeaders -> httpHeaders.setBasicAuth(this.apiKeyDecryptor.decrypt(apiKey), ""))
-            .body(new LinkedMultiValueMap<String, String>() {
+                .post(URI.create("https://api.voicetext.jp/v1/tts"))
+                .headers(httpHeaders -> httpHeaders.setBasicAuth(this.apiKeyDecryptor.decrypt(apiKey), ""))
+                .body(new LinkedMultiValueMap<String, String>() {
 
-                {
-                    add("text", text);
-                    add("speaker", speaker.replace("\"", ""));
-                    if (!StringUtils.isEmpty(emotion)) {
-                        add("emotion", emotion.replace("\"", ""));
+                    {
+                        final String speakerText = speaker.replace("\"", "");
+                        add("text", text);
+                        add("speaker", speakerText);
+                        if (!speakerText.equalsIgnoreCase(Syaberu.SHOW) && !StringUtils.isEmpty(emotion)) {
+                            add("emotion", emotion.replace("\"", ""));
+                        }
+                        add("format", "mp3");
                     }
-                    add("format", "mp3");
-                }
-            });
+                });
         final byte[] body = restTemplate.exchange(request, byte[].class).getBody();
         return Base64.getEncoder().encode(body);
     }
